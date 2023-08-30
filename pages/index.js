0195003beeffeb444a7425133898bf2eb61d23b0
Eduardo-Bilibio <135467658+Eduardo-Bilibio@users.noreply.github.com>
2023-08-30 10:49:04 -0300

    fim

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador />
+            <div>teste</div>
+        </div>
+
+    );
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function Adicionar() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div> 
+            <div>{contador}</div>
+            <button onClick={Adicionar}>Adicionar</button>
+        </div>
+    );
+}
+
+export default Home;
